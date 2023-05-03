253e162eee72f506a17acfc366bff0c2d91fceb1
Jac22 <jacopo.prudentino@gmail.com>
2023-05-03 15:17:51 +0200

    In index le pizze vengono passate come Model

---
+++ project/Controllers/PizzaController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using project.Models;
+
+namespace project.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(Listino.Pizze);
+        }
+    }
+}
